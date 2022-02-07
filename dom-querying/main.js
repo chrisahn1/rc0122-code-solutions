b8f06a9e97f3a5ae8f5623b80a60510737edf505
chrisahn90@yahoo.com <chrisahn90@yahoo.com>
2022-02-07 10:20:58 -0800

    Completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+var element1 = document.querySelector('h1');
+console.log('element1: ', element1);
+// console.dir(element1);
+
+var element2 = document.querySelector('#explanation');
+console.log('element2: ', element2);
+// console.dir(element2);
+
+var element3 = document.querySelector('.hint');
+console.log('element3: ', element3);
+// console.dir(element3);
+
+var elements1 = document.querySelectorAll('p');
+console.log('elements1: ', elements1);
+
+var elements2 = document.querySelectorAll('.example-link');
+console.log('elements2: ', elements2);
