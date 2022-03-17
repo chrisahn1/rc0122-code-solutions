ee8909d5c99555c88b7e59a0ecbb2dd8a6f438d1
Chris Ahn <chrisahn90@yahoo.com>
2022-03-17 14:55:44 -0700

    Fixing readfile

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -1,15 +1,14 @@
+const fs = require('fs');
 const filename = process.argv[2];
 
-const fs = require('fs');
-
 if (filename === 'cunningham.txt') {
-  fs.readFile(`../rc0122-code-solutions/node-fs-readfile/${filename}`, 'utf8', callBack);
+  fs.readFile(filename, 'utf8', callBack);
 } else if (filename === 'dijkstra.txt') {
-  fs.readFile(`../rc0122-code-solutions/node-fs-readfile/${filename}`, 'utf8', callBack);
+  fs.readFile(filename, 'utf8', callBack);
 } else if (filename === 'hipster-ipsum.txt') {
-  fs.readFile(`../rc0122-code-solutions/node-fs-readfile/${filename}`, 'utf8', callBack);
+  fs.readFile(filename, 'utf8', callBack);
 } else if (filename === 'hopper.txt') {
-  fs.readFile(`../rc0122-code-solutions/node-fs-readfile/${filename}`, 'utf8', callBack);
+  fs.readFile(filename, 'utf8', callBack);
 } else {
   console.log('INVALID');
 }
