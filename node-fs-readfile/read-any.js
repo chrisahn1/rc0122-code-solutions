04b620a18de914b98e3c934eb539e84ffb2d719b
Chris Ahn <chrisahn90@yahoo.com>
2022-03-18 10:38:15 -0700

    Completed node-fs-readfile 3

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -1,11 +1,7 @@
 const fs = require('fs');
 const filename = process.argv[2];
 
-if (filename === 'cunningham.txt' || filename === 'dijkstra.txt' || filename === 'hipster-ipsum.txt' || filename === 'hopper.txt') {
-  fs.readFile(filename, 'utf8', callBack);
-} else {
-  console.log('INVALID');
-}
+fs.readFile(filename, 'utf8', callBack);
 
 function callBack(err, data) {
   if (err) throw err;
