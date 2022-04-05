a3479416f58d3735ca936965302166779f041f96
Chris Ahn <chrisahn90@yahoo.com>
2022-04-04 23:25:56 -0700

    In progress react hot button component

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import HotButton from './index';
 
-function HotButton(props) {
-  return (<button onClick={props.onClick} className={props.className }>{props.text}</button >);
+class ColorButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render() {
+    const clickCount = this.state.count;
+    let btnClass;
+    if (clickCount < 3) {
+      btnClass = 'freeze';
+    } else if (clickCount < 6) {
+      btnClass = 'cold';
+    } else if (clickCount < 9) {
+      btnClass = 'cool';
+    } else if (clickCount < 12) {
+      btnClass = 'tepid';
+    } else if (clickCount < 15) {
+      btnClass = 'warm';
+    } else if (clickCount < 18) {
+      btnClass = 'hot';
+    } else {
+      btnClass = 'nuclear';
+    }
+    return <HotButton onClick={this.handleClick} className={`hot-button ${btnClass}`} text="Hot Button" />;
+  }
 }
 
-export default HotButton;
+ReactDOM.render(<ColorButton />, document.querySelector('#root'));
--- react-hot-button-component/src/index.jsx
+++ react-hot-button-component/src/index.jsx
@@ -1,38 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import HotButton from './hot-button';
 
-class ColorButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState({ count: this.state.count + 1 });
-  }
-
-  render() {
-    const clickCount = this.state.count;
-    let button;
-    if (clickCount < 3) {
-      button = <HotButton onClick={this.handleClick} className="hot-button freeze" text="Hot Button" />;
-    } else if (clickCount < 6) {
-      button = <HotButton onClick={this.handleClick} className="hot-button cold" text="Hot Button" />;
-    } else if (clickCount < 9) {
-      button = <HotButton onClick={this.handleClick} className="hot-button cool" text="Hot Button" />;
-    } else if (clickCount < 12) {
-      button = <HotButton onClick={this.handleClick} className="hot-button tepid" text="Hot Button" />;
-    } else if (clickCount < 15) {
-      button = <HotButton onClick={this.handleClick} className="hot-button warm" text="Hot Button" />;
-    } else if (clickCount < 18) {
-      button = <HotButton onClick={this.handleClick} className="hot-button hot" text="Hot Button" />;
-    } else {
-      button = <HotButton onClick={this.handleClick} className="hot-button nuclear" text="Hot Button" />;
-    }
-    return button;
-  }
+function HotButton(props) {
+  return (<button onClick={props.onClick} className={props.className}>{props.text}</button >);
 }
 
-ReactDOM.render(<ColorButton />, document.querySelector('#root'));
+export default HotButton;
