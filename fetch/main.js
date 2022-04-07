532dcf0ff81c5444d6c5b917d5f5b20df1c182da
Chris Ahn <chrisahn90@yahoo.com>
2022-04-06 17:45:57 -0700

    Updated fetch

--- fetch/main.js
+++ fetch/main.js
@@ -1,9 +1,8 @@
-const element = document.querySelector('.testing');
-
 fetch('https://pokeapi.co/api/v2/pokemon/1')
   .then(response => {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    return response.json(element).then(data => console.log(data));
-  });
+    return response.json();
+  })
+  .then(data => console.log(data));
