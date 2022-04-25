a8ac852124f64bd4930aad20c1587ebdcc5e0c9d
Chris Ahn <chrisahn90@yahoo.com>
2022-04-25 16:54:30 -0700

    Updated big o notation 3

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -28,17 +28,17 @@ function uniqueQuadratic(words) {
     let isUnique = true;            // 1 * n = O(n)
     for (
       let c = 0;                    // 1 * n = O(n)
-      c < i;                        // 1 * n = O(n)
-      c++                           // n * n = O(n^2)
+      c < i;                        // 1 * n^2 = O(n^2)
+      c++                           // 2 * n^2 = O(2n^2)
     ) {
-      const comparing = words[c];   // n * n = O(n^2)
-      if (comparing === word) {     // n * n = O(n^2)
-        isUnique = false;           // n * n = O(n^2)
+      const comparing = words[c];   // 2 * n^2 = O(2n^2)
+      if (comparing === word) {     // 1 * n^2 = O(n^2)
+        isUnique = false;           // 1 * n^2 = O(n^2)
       }
     }
     if (isUnique) {                 // 1 * n = O(n)
       unique[unique.length] = word; // 2 * n = O(2n)
     }
   }
-  return unique;                    // 1 * n = O(n)
+  return unique;                    // 1 * 1 = O(1)
 } // Big O Notation for uniqueQuadratic: O(n^2)
