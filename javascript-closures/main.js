e5d7fc9c82f423ce63f2def935b82608660a7ce2
Chris Ahn <chrisahn90@yahoo.com>
2022-04-27 11:31:21 -0700

    Completed js closures

---
+++ javascript-closures/main.js
@@ -0,0 +1,14 @@
+function graduate(credential) {
+  return function (fullName) {
+    return fullName + ', ' + credential;
+  };
+}
+
+const medicalSchool = graduate('M.D.');
+const lawSchool = graduate('Esq');
+
+console.log('medicalSchool: ', medicalSchool('Dan Abramov'));
+console.log('lawSchool: ', lawSchool('Ryan Florence'));
+
+console.dir(medicalSchool);
+console.dir(lawSchool);
